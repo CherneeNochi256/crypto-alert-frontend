ed7af54f7b3f75c9a264cd6ab58ce5673028b56b
Максим <maks3d47@gmail.com>
2023-08-18 20:31:15 +0300

    when i deploy first page that is shown isn't the homepage and url of the home page isn't https://cherneenochi256.github.io/crypto-alert-frontend/ but https://cherneenochi256.github.io

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Routes>
-            <Route index element={<Home/>}/>
+            <Route path={'/'} element={<Home/>}/>
             <Route path={'/:id'} element={<Show/>}/>
           </Routes>
         </BrowserRouter>
