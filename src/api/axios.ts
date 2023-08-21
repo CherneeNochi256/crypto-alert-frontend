2ccf75990468bcd259aa4d1a3e3466403c1e7f57
Максим <maks3d47@gmail.com>
2023-08-21 22:56:57 +0300

    updated backend base url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:8080/api/v1/'
+// const BASE_URL = 'http://localhost:8080/api/v1/' for dev
+const BASE_URL = 'https://crypto-alerts-backend.onrender.com/api/v1/'
 const COIN_GECKO_BASE_URL = 'https://api.coingecko.com/api/v3/'
 const COIN_NEWS_BASE_URL = 'https://newsdata.io/api/1/'
 
